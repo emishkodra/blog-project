db2a2fc91ba1cfe20feb9f7f3ca831f865b0f940
emiiiiiiiiiiiiiii <shkodraemi@gmail.com>
2022-05-15 17:29:03 +0200

    code clean-up

--- blog/src/main/java/com/dev/dao/UserDAO.java
+++ blog/src/main/java/com/dev/dao/UserDAO.java
@@ -3,21 +3,15 @@ package com.dev.dao;
 import com.dev.model.User;
 import com.dev.dao.repository.SoftDeleteRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserDAO extends SoftDeleteRepository<User, Long> {
-    @Query(value = "select u.* from users u \n" +
-            "WHERE u.enabled is not false\n" ,
-            nativeQuery = true
-    )
-    List<User> findAllActiveUsers();
+//    @Query(value = "select u.* from users u \n" +
+//            "WHERE u.enabled is not false\n" ,
+//            nativeQuery = true
+//    )
+//    List<User> findAllActiveUsers();
 
     List<User> findAllByOrderByIdDesc();
-
-    User findByUsername (String username);
-
-
 }
--- blog/src/main/java/com/dev/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dev.exception;
-
-public class PostNotFoundException extends RuntimeException{
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
